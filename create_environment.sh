7db2640ebde9b634bfa937f5216e1ed7da263d9b
PIANEZZE Joris <joris.pianezze@cnrs.fr>
2025-07-11 12:00:42 +0200

    Add script to detect environment

---
+++ create_environment.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ---------------------------------------
+#   Detect machine automaticaly
+# ---------------------------------------
+
+if [[ ${HOSTNAME} = 'belenos'* ]]; then
+
+  export machine='belenos'
+
+elif [[ ${HOSTNAME} = 'olympe'* ]]; then
+
+  export machine='olympe'
+
+elif [[ ${HOSTNAME} = 'nuwa' ]]; then
+
+  export machine='nuwa'
+
+elif [[ ${HOSTNAME} = 'LALL224858' ]]; then
+ 
+  export machine='laptop_joris'
+
+fi
+
+# ---------------------------------------
+#   Link environment file
+# ---------------------------------------
+
+echo ' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '
+if [ -e environment/${machine}/environment.sh ]; then
+  echo '  You are running on '${machine}' with '
+  echo '      following environment :          '
+  echo '                                       '
+  ln -sf environment/${machine}/environment.sh .
+  cat environment.sh
+else
+  echo '  You are running on '${machine}' and'
+  echo '    this machine is not tested yet   '
+fi
+echo ' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '
