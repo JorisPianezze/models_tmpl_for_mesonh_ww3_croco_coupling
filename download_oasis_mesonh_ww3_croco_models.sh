5c1dcb11636801791b57b9ef71eb4d2cee2f0d68
PIANEZZE Joris <joris.pianezze@cnrs.fr>
2025-10-23 15:07:45 +0200

    Add XIOS

--- download_oasis_mesonh_ww3_croco_models.sh
+++ download_oasis_mesonh_ww3_croco_models.sh
@@ -15,6 +15,7 @@ export download_oasis=true
 export download_mesonh=true
 export download_croco=true
 export download_ww3=true
+export download_xios=true
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #   Download oasis3-mct_${version_oasis}
@@ -45,10 +46,10 @@ if [ ${download_mesonh} = true ]; then
   if [[ ! -d MNH-V${version_mesonh} ]]
   then
     git clone https://src.koda.cnrs.fr/mesonh/mesonh-code.git
-    cd mesonh-code
-    git checkout PACK-MNH-V${version_mesonh}
-    cp ../environment/${machine}/compilation_mesonh/Makefile.MESONH.mk src/
-    cd ..
+    #cd mesonh-code
+    #git checkout PACK-MNH-V${version_mesonh}
+    #cp ../environment/${machine}/compilation_mesonh/Makefile.MESONH.mk src/
+    #cd ..
     mv mesonh-code MNH-V${version_mesonh}
   else
     echo '  MNH-V'${version_mesonh}' directory already exists.'
@@ -99,6 +100,24 @@ if [ ${download_ww3} = true ]; then
     mv WW3 WW3-v${version_ww3}
   else
     echo '  WW3-v'${version_ww3}' directory already exists.'
+  fi
+
+fi
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   Download xios-${version_xios}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if [ ${download_xios} = true ]; then
+
+  if [[ ! -d xios-${version_xios} ]]
+  then
+    svn co http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS3/trunk xios-3.0-trunk
+    cp environment/${machine}/compilation_xios/arch-${machine}.env xios-3.0-trunk/arch/
+    cp environment/${machine}/compilation_xios/arch-${machine}.fcm xios-3.0-trunk/arch/
+    cp environment/${machine}/compilation_xios/arch-${machine}.path xios-3.0-trunk/arch/
+  else
+    echo '  xios-'${version_xios}' directory already exists.'
   fi
 
 fi
--- environment/laptop_joris/environment.sh
+++ environment/laptop_joris/environment.sh
@@ -10,9 +10,12 @@ export version_oasis='5.2'
 export version_mesonh='5-7-2'
 export version_croco='2.1.0'
 export version_ww3='7.14'
+export version_xios='3.0-trunk'
 
 export CC=mpicc
 export FC=mpif90
 
 export NETCDF_CONFIG=/home/piaj/03_workdir/2B_devel_MNH_CROCO_XIOS/models_TUTORIAL/libraries/build_netcdf-${version_netcdf_fortran}/bin/nf-config
 export OASISDIR=/home/piaj/03_workdir/2B_devel_MNH_CROCO_XIOS/models_TUTORIAL/oasis3-mct_5.2/build_oa3-mct
+export XIOS_DIR=/home/piaj/03_workdir/2B_devel_MNH_CROCO_XIOS/models_TUTORIAL/xios-3.0-trunk
+export XIOS_DIR_OASIS=/home/piaj/03_workdir/2B_devel_MNH_CROCO_XIOS/models_TUTORIAL/xios-3.0-trunk_oasis3-mct_5.2
